5eed5ebd16a9aa0b46b700fcfdbf51037917e908
dudascolari <143437679+dudascolari@users.noreply.github.com>
2024-03-12 14:34:53 -0300

    Add files via upload

---
+++ type-number.js
@@ -0,0 +1,24 @@
+// tipo number
+
+const meunumero = 3;
+
+const primerionumero = 1;
+const segundonumero =  2;
+
+const operacaomatematica = primerionumero + segundonumero;
+
+console.log(operacaomatematica)
+
+// ponto flutuante
+
+const numeropontoflutuante = 3.3;
+const pontoflutuantezero = .5;
+
+const novaoperacao = primerionumero / numeropontoflutuante; 
+
+console.log(novaoperacao)
+
+// NaN -> Not a Numer (não é um numero)
+
+const alura = "Alura";
+console.log(alura * primerionumero)
---
+++ type-string.js
@@ -0,0 +1,14 @@
+const testo1 = "Ola, mundo";
+const texto2 = 'ola, mundo';
+const senha = "senhaupersegura456!";
+const stringdenumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meunome = "Eduarda"
+console.log(citacao)
+
+// concentracao (+)
+
+console.log(citacao + meunome)
+
+// template string ou template literal
